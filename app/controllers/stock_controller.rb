46238170f47d3e5f69b24353a81ec6ae9272ee4a
arthurtruong <arthurtruong.work@gmail.com>
2018-06-01 20:05:35 +1000

    modified controller and models, search function for stocks now work

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,4 +1,8 @@
 class StockController < ApplicationController
   def search
+
+  @stock = Stock.new_from_lookup(params[:stock])
+  render stock_search_path
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'my_portfolio', to: "user#my_portfolio"
-  get 'stock/search', to: 'search#stocks'
+  get 'stock/search', to: 'stock#search'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
