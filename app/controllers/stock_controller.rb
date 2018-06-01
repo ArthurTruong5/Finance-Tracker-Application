30c9aa568a817784decb5e873ce4ce06fb0b5654
arthurtruong <arthurtruong.work@gmail.com>
2018-06-01 20:53:59 +1000

    added error handling for stock search

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,8 +1,17 @@
 class StockController < ApplicationController
   def search
-
+    if params[:stock].present?
   @stock = Stock.new_from_lookup(params[:stock])
+if @stock
   render stock_search_path
+else
+  flash[:danger] = "Search is invald, please enter the correct symbol"
+  redirect_to my_portfolio_path
+end
 
+else
+  flash[:danger] = "Search is invalid, please enter the correct string."
+  redirect_to my_portfolio_path
   end
+end
 end
